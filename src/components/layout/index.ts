fb496086f2d5b77616ea46cc0086f4a480ee6e40
denisds73 <flaviodenis2003@gmail.com>
2026-06-19 00:34:25 +0530

    fix: remove deleted CustomCursor export

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,4 +1,3 @@
 export { default as Navbar } from './Navbar'
 export { default as ScrollProgress } from './ScrollProgress'
-export { default as CustomCursor } from './CustomCursor'
 export { default as Section } from './Section'
